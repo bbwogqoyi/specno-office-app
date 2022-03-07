ca54ac47cd87edbde526f0486a12dddb3681f2c0
Bonani Brian Wogqoyi <bonani.wogqoyi@outlook.com>
2022-03-07 12:11:12 +0200

    feature: added the initial design of the office overview component

--- packages/web/src/App.vue
+++ packages/web/src/App.vue
@@ -2,13 +2,29 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import OfficeOverview from './components/OfficeOverview.vue'
 </script>
 
 <template>
-<div class="bg-indigo-50 flex flex-col items-center h-screen justify-center">
+<!-- <div class="bg-indigo-50 flex flex-col items-center h-screen justify-center">
   <img classalt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+</div> -->
+
+<div class="h-screen flex items-center bg-[#F8FAFC] ">
+  <div class="w-1/2 mx-auto ">
+    <OfficeOverview 
+      name="Specno"
+      capacity=25
+      noOfOccupants=5
+      mobile="082&nbsp;364&nbsp;9864" 
+      email="info@specno.com" 
+      location="10 Willie Van Schoor Dr, Bo Oakdale, Cape Town, 7530"
+      ribbonColour="#97512C"
+    />
+  </div>
 </div>
+
 
 </template>
 
