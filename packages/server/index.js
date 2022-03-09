4a695012fb2e6ddb8d7aa56ec6871d933eb5b2d5
Bonani Brian Wogqoyi <bonani.wogqoyi@outlook.com>
2022-03-09 20:18:03 +0200

    feature: added the initial backend scaffolding

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,1 +1,21 @@
-console.log("@specno/server - started")
+import express from 'express';
+import yenv from 'yenv';
+import cors from 'cors';
+
+
+const app = express()
+const env = yenv('env.yaml', { env: 'development' })
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(env.PORT, () => {
+  console.log(`Server listening on port ${env.PORT}`)
+})
