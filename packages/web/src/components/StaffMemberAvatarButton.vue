a808c785ce5e654c08b60889ba0a405209e8ed59
Bonani Brian Wogqoyi <bonani.wogqoyi@outlook.com>
2022-03-11 06:18:38 +0200

    feature: added the avatar picker for the staff member view

---
+++ packages/web/src/components/StaffMemberAvatarButton.vue
@@ -0,0 +1,24 @@
+<script setup> 
+import { defineEmits, toRefs } from "vue";
+
+const props = defineProps({
+  selectedAvatar: String,
+  componentAvatar: String
+});
+
+const { componentAvatar, selectedAvatar } = toRefs(props);
+const isSelected = () => selectedAvatar.value === componentAvatar.value 
+const getAvatarPath = () => `../src/assets/${componentAvatar.value}`
+
+const emit = defineEmits(['change'])
+const changeSelectedAvatar = () => emit('change', componentAvatar.value)
+
+</script>
+
+<template>
+  <button type="button" @click="changeSelectedAvatar"
+    class="rounded-full w-16"
+    :class="[isSelected() ? 'border-4 border-black':'border-none']" >
+    <img class="w-16" :src="getAvatarPath()" />
+  </button>
+</template>
