29b3a553275975b528ce186a1c5f64ac7bdc70b6
Bonani Brian Wogqoyi <bonani.wogqoyi@outlook.com>
2022-03-11 06:17:04 +0200

    feature: added a new component - the floating add button on the bottom left corner of the pages

---
+++ packages/web/src/components/AddButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="absolute cursor-pointer right-4 bottom-4 bg-[#0D4477] h-14 w-14 rounded-full flex items-center justify-center" >
+    <svg class="w-10 h-10" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M20 9.65a.75.75 0 0 1 .75.75v8.85h8.85a.75.75 0 0 1 0 1.5h-8.85v8.85a.75.75 0 0 1-1.5 0v-8.85H10.4a.75.75 0 0 1 0-1.5h8.85V10.4a.75.75 0 0 1 .75-.75Z" fill="#F8FAFC"/></svg>
+  </div>
+</template>
